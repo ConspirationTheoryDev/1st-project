c2e67c751dfa7a43a072f8a098313b869a5a4a5f
DESKTOP-A3VO10O\Axel Duza Knaga <trotes15@vp.pl>
2023-10-04 20:35:54 +0200

    config Vite, Ts, Priettier, Antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ConfigProvider } from 'antd'
+import { Button } from 'antd'
 
-function App() {
-  const [count, setCount] = useState(0)
+import styles from './App.module.scss'
 
+import '@/styles/index.scss'
+
+const App = (): JSX.Element => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <ConfigProvider>
+      <main className={styles.mainContainer}>
+        <menu>
+          <ul>
+            <Button type={'primary'}>Strona główna</Button>
+            <Button type={'primary'}>Kontakty</Button>
+            <Button type={'primary'}>Produkty</Button>
+            <Button type={'primary'}>Lokalne sklepy</Button>
+          </ul>
+        </menu>
+
+        <section>Zawartość strony głównej</section>
+      </main>
+    </ConfigProvider>
   )
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+import path from 'path'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  server: { port: 3000 },
+
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src/'),
+      routes: `${path.resolve(__dirname, './src/routes/')}`,
+      store: `${path.resolve(__dirname, './src/store/')}`,
+      utils: `${path.resolve(__dirname, './src/utils/')}`,
+      api: `${path.resolve(__dirname, './src/api/')}`,
+      assets: `${path.resolve(__dirname, './src/assets/')}`,
+      theme: `${path.resolve(__dirname, './src/theme/')}`,
+      components: `${path.resolve(__dirname, './src/components/')}`,
+    },
+  },
 })
